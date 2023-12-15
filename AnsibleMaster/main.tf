0831d066d187d0bc63aa3cd869bcf19065401e25
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-12-15 13:06:54 +0100

    Update main.tf

--- AnsibleMaster/main.tf
+++ AnsibleMaster/main.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "AnsibleMaster" {
 	tags = {
 	    # The count.index allows you to launch a resource 
 	    # starting with the distinct index number 0 and corresponding to this instance.
-	    Name = "KubernetesMaster-${count.index}"
+	    Name = "AnsibleMaster-${count.index}"
   	}
 user_data = <<EOF
 #! /bin/bash
@@ -31,16 +31,5 @@ sudo sh /home/ansiuser/simplilearn_aws4kubernetes/AnsibleMaster/master_config_ru
 sudo su - -c 'su - ansiuser -c /home/ansiuser/simplilearn_aws4kubernetes/AnsibleMaster/master_config_run_as_ansiuser.sh' >>/var/tmp/ansiuser.log 2>&1
 # autoinstall
 sed -i "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
-## Install Docker
-sudo wget https://raw.githubusercontent.com/lerndevops/labs/master/scripts/installDocker.sh -P /tmp
-sudo chmod 755 /tmp/installDocker.sh
-sudo bash /tmp/installDocker.sh
-## Install kubeadm,kubelet,kubectl
-sudo wget https://raw.githubusercontent.com/lerndevops/labs/master/scripts/installK8S-v1-23.sh -P /tmp
-sudo chmod 755 /tmp/installK8S-v1-23.sh
-sudo bash /tmp/installK8S-v1-23.sh
-sudo wget https://raw.githubusercontent.com/lerndevops/labs/master/kubernetes/0-install/daemon.json -P /etc/docker
-sudo systemctl restart docker.service
-sudo su - -c 'su - ansiuser -c /home/ansiuser/simplilearn_aws4kubernetes/AnsibleMaster/kubernetes_config_master_node.sh' >>/var/tmp/ansiuser.log 2>&1
 EOF
 }
