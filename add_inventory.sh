6963955b3a2c69bc87f0867b15c6ba14d647930f
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-12-15 18:38:28 +0100

    Delete add_inventory.sh

--- add_inventory.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cp /etc/ansible/ansible.cfg /etc/ansible/ansible.cfg.bak
-cat > /etc/ansible/ansible.cfg << EOF
-[defaults]
-inventory = /home/ansiuser/myinventory
-EOF
-diff /etc/ansible/ansible.cfg /etc/ansible/ansible.cfg.bak
-cat /etc/ansible/ansible.cfg
-#
