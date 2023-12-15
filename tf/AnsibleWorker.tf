688eae3597eb9a4ec5083cbda1ff5f8a7150e584
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-12-15 12:47:36 +0100

    Update AnsibleWorker.tf

--- tf/AnsibleWorker.tf
+++ tf/AnsibleWorker.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "bh67" {
-	count = 1
+	count = 2
 	ami 				= var.ec2_parameters.ami
 	instance_type 			= var.ec2_parameters.itype
 	subnet_id 			= var.subnet_id
@@ -13,7 +13,7 @@ resource "aws_instance" "bh67" {
 	tags = {
 	    # The count.index allows you to launch a resource 
 	    # starting with the distinct index number 0 and corresponding to this instance.
-	    Name = "Kubernetes-${count.index}"
+	    Name = "AnsibleWorker-${count.index}"
   	}
 user_data = <<EOF
 #! /bin/bash
@@ -36,23 +36,10 @@ sudo diff /etc/sudoers /etc/sudoers.bak >>/var/tmp/yum.update 2>&1
 sudo apt install -y awscli >>/var/tmp/yum.update 2>&1
 sudo ansible-galaxy collection install amazon.aws -y >>/var/tmp/yum.update 2>&1
 sudo apt install python3-pip -y >>/var/tmp/yum.update 2>&1
-echo `hostname -I` > ip_`hostname`.txt
-aws ec2 describe-instances --filters 'Name=tag:Name,Values=*' >> ip_`hostname`.txt
-aws s3 cp ip_`hostname`.txt s3://bh67-githubactions-bucket/
 sudo su - -c 'su - ansiuser -c "git clone https://github.com/Bh67tablet/simplilearn_aws4kubernetes.git"' >>/var/tmp/yum.update 2>&1
 sudo chmod 755 /home/ansiuser/simplilearn_aws4kubernetes/AnsibleMaster/*.sh >>/var/tmp/yum.update 2>&1
 # autoinstall
 sed -i "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
-## Install Docker
-sudo wget https://raw.githubusercontent.com/lerndevops/labs/master/scripts/installDocker.sh -P /tmp
-sudo chmod 755 /tmp/installDocker.sh
-sudo bash /tmp/installDocker.sh
-## Install kubeadm,kubelet,kubectl
-sudo wget https://raw.githubusercontent.com/lerndevops/labs/master/scripts/installK8S-v1-23.sh -P /tmp
-sudo chmod 755 /tmp/installK8S-v1-23.sh
-sudo bash /tmp/installK8S-v1-23.sh
-sudo wget https://raw.githubusercontent.com/lerndevops/labs/master/kubernetes/0-install/daemon.json -P /etc/docker
-sudo systemctl restart docker.service
 EOF
 
   vpc_security_group_ids = [
