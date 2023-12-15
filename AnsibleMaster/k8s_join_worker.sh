f7ca4459952d482fbe4164bb8911d62bceb63a45
Bh67tablet <bh67tablet@gmail.com>
2023-12-15 18:56:15 +0100

    sort

--- AnsibleMaster/k8s_join_worker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo wget https://raw.githubusercontent.com/lerndevops/labs/master/kubernetes/0-install/daemon.json -P /etc/docker
-sudo systemctl restart docker.service
-# sudo "join kommand from master"
--- AnsibleMaster/k8s_reset.sh
+++
@@ -1,3 +0,0 @@
-kubectl delete -f https://raw.githubusercontent.com/projectcalico/calico/v3.24.1/manifests/calico.yaml
-sudo kubeadm reset --force
-sudo rm -rf /var/lib/kubelet /etc/kubernetes /var/lib/etcd $HOME/.kube
