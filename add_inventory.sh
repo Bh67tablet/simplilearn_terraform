c843947c0dfa33ff6176dd95289dd456d132c081
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-12-15 11:52:43 +0100

    Update and rename add_user_ansible.sh to add_inventory.sh

---
+++ add_inventory.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cp /etc/ansible/ansible.cfg /etc/ansible/ansible.cfg.bak
+cat > /etc/ansible/ansible.cfg << EOF
+[defaults]
+inventory = /home/ansiuser/myinventory
+EOF
+diff /etc/ansible/ansible.cfg /etc/ansible/ansible.cfg.bak
+cat /etc/ansible/ansible.cfg
+#
