4af3fae571b8b822f36fdab5073b641490f5efbe
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-12-15 11:51:34 +0100

    Update install_python_ansible_add_user_ansible.sh

--- install_python_ansible_add_user_ansible.sh
+++ install_python_ansible_add_user_ansible.sh
@@ -10,3 +10,10 @@ apt install -y awscli
 ansible-galaxy collection install amazon.aws -y
 apt install python3-pip -y
 #
+sudo useradd -m ansiuser -s /bin/bash -p 'ansiuser'
+sudo echo "ansiuser:ansiuser" | chpasswd
+sudo cp -p /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+sudo cp -p /etc/sudoers /etc/sudoers.bak
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sudo sed -i "s/^root.*$/root    ALL=(ALL:ALL) ALL\nansiuser ALL=NOPASSWD: ALL/g" /etc/sudoers
+sudo systemctl restart sshd
