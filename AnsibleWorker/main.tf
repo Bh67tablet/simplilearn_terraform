8e6e96f0b68b5902a0269ae99642a71d2705e38e
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-12-15 13:19:56 +0100

    Update main.tf

--- AnsibleWorker/main.tf
+++ AnsibleWorker/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "bh67" {
 	count = 2
 	ami 				= var.ec2_parameters.ami
 	instance_type 			= var.ec2_parameters.itype
-	subnet_id 			= var.subnet_id
+	#subnet_id 			= var.subnet_id
 	associate_public_ip_address 	= var.ec2_parameters.publicip
 	key_name 			= var.ec2_parameters.keyname
 	iam_instance_profile   		= var.ec2_parameters.iam_instance_profile
