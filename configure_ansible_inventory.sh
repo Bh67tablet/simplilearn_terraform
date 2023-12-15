56ccccfb849e35d1540ace92332f5f6237370aec
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-12-15 10:56:33 +0100

    Update configure_ansible_inventory.sh

--- configure_ansible_inventory.sh
+++ configure_ansible_inventory.sh
@@ -1,27 +1,4 @@
 #!/bin/bash
-#
-# add IPv4 adresses to /home/ansiuser/ips
-#
-cat > /home/ansiuser/ips << EOF
-172.31.33.219
-172.31.37.180
-EOF
-#
-sudo useradd -m ansiuser -s /bin/bash -p 'ansiuser'
-sudo echo "ansiuser:ansiuser" | chpasswd
-sudo cp -p /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
-sudo cp -p /etc/sudoers /etc/sudoers.bak
-sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-sudo sed -i "s/^root.*$/root    ALL=(ALL:ALL) ALL\nansiuser ALL=NOPASSWD: ALL/g" /etc/sudoers
-sudo systemctl restart sshd
-cp /etc/ansible/ansible.cfg /etc/ansible/ansible.cfg.bak
-cat > /etc/ansible/ansible.cfg << EOF
-[defaults]
-inventory = /home/ansiuser/myinventory
-EOF
-diff /etc/ansible/ansible.cfg /etc/ansible/ansible.cfg.bak
-cat /etc/ansible/ansible.cfg
-#
 echo [webserver] > /home/ansiuser/myinventory
 for ip in $(cat /home/ansiuser/ips); do echo $ip >> /home/ansiuser/myinventory; done
 #
