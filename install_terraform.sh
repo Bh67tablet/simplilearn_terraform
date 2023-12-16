4943c7a3db23b6e1f13b69abacb97c92429706a6
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-12-16 23:11:52 +0100

    Update install_terraform.sh

--- install_terraform.sh
+++ install_terraform.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 install_ubuntu() {
-  sudo apt -y update
-  sudo apt -y install unzip
-  sudo apt -y install git
-  sudo wget https://releases.hashicorp.com/terraform/1.6.3/terraform_1.6.3_linux_amd64.zip
-  sudo unzip terraform_1.6.3_linux_amd64.zip
-  sudo mv terraform /usr/bin
-  exit 0
+	sudo apt -y update
+	sudo apt -y install unzip
+	sudo apt -y install git
+	sudo wget https://releases.hashicorp.com/terraform/1.6.3/terraform_1.6.3_linux_amd64.zip
+	sudo unzip terraform_1.6.3_linux_amd64.zip
+	sudo mv terraform /usr/bin
+	exit 0
 }
 
 install_centos() {
-    sudo yum -y update
-    sudo yum -y install unzip
-    sudo yum -y install git
-    sudo wget https://releases.hashicorp.com/terraform/1.6.3/terraform_1.6.3_linux_amd64.zip
-    sudo unzip terraform_1.6.3_linux_amd64.zip
-    sudo mv terraform /usr/bin
+	sudo yum -y update
+	sudo yum -y install unzip
+	sudo yum -y install git
+	sudo wget https://releases.hashicorp.com/terraform/1.6.3/terraform_1.6.3_linux_amd64.zip
+	sudo unzip terraform_1.6.3_linux_amd64.zip
+	sudo mv terraform /usr/bin
 	exit 0
 }
 ################ MAIN ###################
