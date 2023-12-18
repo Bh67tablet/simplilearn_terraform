9ccaa2a4d4dc6f55175a09c564a679e598ee7cce
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2023-12-18 09:23:03 +0100

    Update run_terraform.sh

--- tf/run_terraform.sh
+++ tf/run_terraform.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 terraform -chdir=AnsibleS3 init 
-terraform -chdir=AnsibleS3 apply --auto-approve
+terraform -chdir=AnsibleS3 destroy --auto-approve
 terraform -chdir=AnsibleIam init 
-terraform -chdir=AnsibleIam apply --auto-approve
+terraform -chdir=AnsibleIam destroy --auto-approve
 terraform -chdir=AnsibleWorker init 
-terraform -chdir=AnsibleWorker apply --auto-approve
+terraform -chdir=AnsibleWorker destroy --auto-approve
 terraform -chdir=AnsibleMaster init
-terraform -chdir=AnsibleMaster apply --auto-approve
+terraform -chdir=AnsibleMaster destroy --auto-approve
