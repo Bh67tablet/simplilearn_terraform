0de575534cc0131e62f6eb4d0eb89a05f1960582
Bh67tablet <122218639+Bh67tablet@users.noreply.github.com>
2024-01-22 13:39:11 +0100

    Update install_terraform.sh

--- tf/install_terraform.sh
+++ tf/install_terraform.sh
@@ -4,8 +4,8 @@ install_ubuntu() {
 	sudo apt -y update
 	sudo apt -y install unzip
 	sudo apt -y install git
-	sudo wget https://releases.hashicorp.com/terraform/1.6.3/terraform_1.6.3_linux_amd64.zip
-	sudo unzip terraform_1.6.3_linux_amd64.zip
+ 	sudo terraform_latest=$( curl -s https://releases.hashicorp.com/terraform/ | sed "s|.*\">||g" | egrep "^terraform_[0-9]+\.[0-9]+\.[0-9]+</a>$" | sed "s|</a>||g;s|^terraform_||g" | head -1 ) && wget https://releases.hashicorp.com/terraform/${terraform_latest}/terraform_${terraform_latest}_linux_amd64.zip
+	sudo unzip terraform_${terraform_latest}_linux_amd64.zip
 	sudo mv terraform /usr/bin
 	exit 0
 }
@@ -14,8 +14,8 @@ install_centos() {
 	sudo yum -y update
 	sudo yum -y install unzip
 	sudo yum -y install git
-	sudo wget https://releases.hashicorp.com/terraform/1.6.3/terraform_1.6.3_linux_amd64.zip
-	sudo unzip terraform_1.6.3_linux_amd64.zip
+ 	sudo terraform_latest=$( curl -s https://releases.hashicorp.com/terraform/ | sed "s|.*\">||g" | egrep "^terraform_[0-9]+\.[0-9]+\.[0-9]+</a>$" | sed "s|</a>||g;s|^terraform_||g" | head -1 ) && wget https://releases.hashicorp.com/terraform/${terraform_latest}/terraform_${terraform_latest}_linux_amd64.zip
+	sudo unzip terraform_${terraform_latest}_linux_amd64.zip
 	sudo mv terraform /usr/bin
 	exit 0
 }
